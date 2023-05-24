7c7978f4a7a4e45eeb9b5374da4f1df5d0a9a616
TomasBankauskas <tbdizainas@gmail.com>
2023-05-24 15:43:14 +0300

    updated stackbit.config.ts, added netlify.toml

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -7,6 +7,8 @@ import { stats } from './.stackbit/models/stats'
 
 export default defineStackbitConfig({
   stackbitVersion: '~0.6.0',
+  ssgName: 'nextjs',
+  nodeVersion: '16',
   contentSources: [
     new GitContentSource({
       rootPath: __dirname,
